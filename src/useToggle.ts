3deafc11b88dc4cd7eecc04fc010a31604d13497
Nils Nathorst-Windahl <nils.nathorstwindahl@antura.se>
2021-10-29 16:41:08 +0200

    add linting

--- src/useToggle.ts
+++ src/useToggle.ts
@@ -1,12 +1,12 @@
-import { useState, useCallback }  from "react";
+import { useState, useCallback } from "react";
 
 export const useToggle = (initialState: boolean) => {
-    const [isToggled, setIsToggled] = useState(initialState);
-  
-        const toggle = useCallback(
-      () => setIsToggled(state => !state),
-      [setIsToggled],
-    );
-  
-    return [isToggled, toggle] as const;
-  }
+  const [isToggled, setIsToggled] = useState(initialState);
+
+  const toggle = useCallback(
+    () => setIsToggled((state) => !state),
+    [setIsToggled]
+  );
+
+  return [isToggled, toggle] as const;
+};
